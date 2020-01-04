c50d60256390bf63fc4fed16922c2b6f80224fc2
Ricky <ricky@pop-os.localdomain>
2020-01-04 17:31:45 -0500

    style menu item, directoriy

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+// the watch store that andrew always wanted
+//  to make but cannot do
+import React from "react";
+import Directory from "../../components/directory/directory.component.jsx";
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
