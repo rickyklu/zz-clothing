26b1c5f1484009e9419821ae4faf871c1957ef2e
Ricky <rickyklu@live.com>
2020-01-06 18:43:14 -0500

    add signin page, input form field, custom button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInRegister from "./pages/signin-register/signin-register.component.jsx";
+import Header from "./components/header/header.component.jsx";
+
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInRegister} />
       </Switch>
     </div>
   );
