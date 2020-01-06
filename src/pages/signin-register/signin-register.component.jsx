3c35abf654747194972d1e56e99a4240787cb325
Ricky <rickyklu@live.com>
2020-01-06 18:46:22 -0500

    add signin page, input form field, custom button

---
+++ src/pages/signin-register/signin-register.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component.jsx";
+import "./signin-register.styles.scss";
+
+const SignInRegister = () => {
+  return (
+    <div className='sign-in-an-sign-up'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInRegister;
